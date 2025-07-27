4659436235bbfea08c23b65c4b58c714ec34cec0
Mohd Rahib <rahib110@gmail.com>
2025-07-27 23:34:37 +0800

    modified python file

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
+from fastapi import FastAPI
+import json
 
-# Step 1: Import FastAPI
-from fastapi import FastAPI, Request
-
-# Step 2: Create FastAPI app instance
 app = FastAPI()
 
-# Step 3: Define a GET route
 @app.get("/")
-def read_root():
+def home():
     return {"message": "Welcome to My API"}
 
-# Step 4: Define a GET route with query param
-@app.get("/greet")
-def greet_user(name: str):
-    return {"greeting": f"Hello, {name}!"}
+@app.get("/data")
+def get_json_data():
+    with open("bank_emp.json", "r") as file:
+        data = json.load(file)
+    return data
 
-# Step 5: Define a POST route that accepts JSON
-@app.post("/submit")
-async def submit_data(request: Request):
-    data = await request.json()   # read incoming JSON
-    return {"received": data}
