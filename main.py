c580201112b2caa790e84be533a4b37b2142b897
Mohd Rahib <rahib110@gmail.com>
2025-07-27 23:55:49 +0800

    modified python file

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import json
+import os
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Welcome to My API"}
-
-@app.get("/data")
-def get_json_data():
-    with open("bank_emp.json", "r") as file:
+@app.get("/data/{filename}")
+def get_any_json(filename: str):
+    file_path = f"bank_emp.json"
+    if not os.path.exists(file_path):
+        raise HTTPException(status_code=404, detail="File not found")
+    with open(file_path, "r") as file:
         data = json.load(file)
     return data
 
